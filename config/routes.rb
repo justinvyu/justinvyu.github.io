03faeaf470474080ffc87d4a53b1d220d77ab98c
Justin Yu <justin.v.yu@gmail.com>
2016-03-12 23:22:09 -0800

    Fix up about#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :posts
+
   get 'contact' => 'contact#index'
   get 'about' => 'about#index'
   root 'welcome#index'
-  resources :posts
+
+  get '*path' => redirect('/')
 end
